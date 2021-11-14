9d0999bcd3d0c2f4b9878efb832dc618db42f9e4
ahmedezzat <mr1.dsgn@hotmail.com>
2021-11-14 14:30:18 +0200

    initial commit

---
+++ PaginateMe/DataPagerExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaginateMe
+{
+    public static class DataPagerExtension
+    {
+        public static async Task<PagedModel<TModel>> PaginateAsync<TModel>(
+            this IQueryable<TModel> query,
+            int page,
+            int limit)
+            where TModel : class
+        {
+            var paged = new PagedModel<TModel>();
+
+            page = page < 0 ? 1 : page;
+
+            paged.CurrentPage = page;
+            paged.PageSize = limit;
+            var totalItemsCountTask = await query.CountAsync();
+
+            var startRow = (page - 1) * limit;
+            paged.Items = await query
+                .Skip(startRow)
+                .Take(limit)
+                .ToListAsync();
+
+            paged.TotalItems = totalItemsCountTask;
+            paged.TotalPages = (int)Math.Ceiling(paged.TotalItems / (double)limit);
+
+            return paged;
+        }
+    }
+
+}
+
---
+++ PaginateMe/PagedModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaginateMe
+{
+    public class PagedModel<TModel>
+    {
+        private const int MaxPageSize = 500;
+        private int _pageSize;
+
+        public PagedModel()
+        {
+            Items = new List<TModel>();
+        }
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public IList<TModel> Items { get; set; }
+    }
+}
